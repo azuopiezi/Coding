dd974be313b18bdbeccf12b9b769fe003e8a15c4
shixingwe.n@163.com <shixingwe.n@163.com>
2020-09-10 15:06:37 +0800

    学习ansible-doc

---
+++ Ansible/ansible.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+ansible-doc 相当于man help
+ansible-doc --help
+Usage: ansible-doc [options] [module...]
+
+Options:
+  -h, --help            show this help message and exit
+  -l, --list            List available modules
+  -M MODULE_PATH, --module-path=MODULE_PATH
+                        specify path(s) to module library (default=None)
+  -s, --snippet         Show playbook snippet for specified module(s)
+  -v, --verbose         verbose mode (-vvv for more, -vvvv to enable
+                        connection debugging)
+  --version             show program's version number and exit
+这个工具非常有用
+
