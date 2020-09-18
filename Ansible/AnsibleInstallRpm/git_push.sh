371615b6b8caf95672ea4a0fa3e942bcc51b685c
azuopiezi <shixingwe.n@163.com>
2020-09-18 15:38:25 +0800

    sd

--- Ansible/AnsibleInstallRpm/git_push.sh
+++ Ansible/AnsibleInstallRpm/git_push.sh
@@ -1,12 +1,14 @@
-#!/bin/bash
+#!/usr/bin/expect -f
 set -x
-read -p "请输入文件路径或者文件目录": Path
 git add .
 read -p "请输入commit 信息": CommitMessage
 git commit -m "${CommitMessage}"
 
 #git 到远端服务器上 需要输入用户名和密码
-echo “username=azuopiezi,password=sX209@^Yj”
-git push
 
-
+spawn git push
+expect {
+  "Username*:" { send "azuopiezi\r";exp_continue }
+  "Password*:" {send "sX209@^Yj\r"}
+ }
+ expect eof
